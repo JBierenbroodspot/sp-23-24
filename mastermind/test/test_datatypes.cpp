621704203f9de74ff9368abc2f1b3d1a2934618c
JBierenbroodspot <jim12.brood@gmail.com>
2023-09-08 00:21:01 +0200

    Add test__allocate_code happy values

--- mastermind/test/test_datatypes.cpp
+++ mastermind/test/test_datatypes.cpp
@@ -1,2 +1,25 @@
+#include "datatypes.h"
+
 #include <gtest/gtest.h>
 
+#include <stdlib.h>
+
+
+TEST(test__allocate_code, handles_happy_values) {
+    size_t input_values[3] = {1, 5, 10};
+    size_t colour_size = sizeof(Colour);
+
+    for (auto &input_value : input_values) {
+        Code* code = _allocate_code(input_value);
+
+        size_t result = sizeof(*code);
+        size_t expected_value = input_value * colour_size;
+
+        EXPECT_EQ(result, expected_value) 
+            << "Allocated size " << result 
+            << " did not match expected size " << expected_value 
+            << ".";
+
+        free(code);
+    }
+}
