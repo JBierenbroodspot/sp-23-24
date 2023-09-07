3bf8509fb4b4cdaaa92a0c5dc438530246e07200
JBierenbroodspot <jim12.brood@gmail.com>
2023-09-08 00:28:15 +0200

    Add test__allocate_feedback happy values

--- mastermind/test/test_datatypes.cpp
+++ mastermind/test/test_datatypes.cpp
@@ -4,6 +4,24 @@
 
 #include <stdlib.h>
 
+TEST(test__allocate_feedback, handles_happy_values) {
+    size_t input_values[3] = {1, 5, 10};
+    size_t feedback_size = sizeof(Feedback);
+
+    for (auto &input_value : input_values) {
+        Feedback* feedback = _allocate_feedback(input_value);
+        
+        size_t result = sizeof(*feedback);
+        size_t expected_value = input_value * feedback_size;
+
+        EXPECT_EQ(result, expected_value)
+            << "Allocated size " << result 
+            << " did not match expected size " << expected_value 
+            << ".";
+
+        free(feedback);
+    }
+}
 
 TEST(test__allocate_code, handles_happy_values) {
     size_t input_values[3] = {1, 5, 10};
