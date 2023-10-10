558454a53e8b61ebee8b0eb9374ca424396cdd63
jimmy <jim12.brood@gmail.com>
2023-10-11 00:51:05 +0200

    Add stderr to _allocate_heap

--- mastermind/src/datatypes/datatypes.c
+++ mastermind/src/datatypes/datatypes.c
@@ -1,11 +1,16 @@
 #include "datatypes.h"
 
+#include <stdio.h>
+
 void *_allocate_heap(size_t type_size, size_t type_amount)
 {
-    void *ptr = malloc(type_size * type_amount);
+    void *ptr = calloc(type_size, type_amount);
 
     if (ptr == NULL)
+    {
+        fprintf(stderr, "Heap allocation failure.");
         exit(EXIT_FAILURE);
+    }
 
     return ptr;
 }
